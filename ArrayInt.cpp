ad927cc21c745ae6fcda964990a6fd648b1ecbb2
cmaste <cmaste148@gmail.com>
2025-10-01 22:38:43 -0700

    Jumbled Mess

--- ArrayInt.cpp
+++ ArrayInt.cpp
@@ -8,76 +8,131 @@
 //
 
 #include "ArrayInt.hpp"
+#include <iostream>
+#include <algorithm>
 #include <stdexcept>
 
 // constructor, set up the array
-ArrayInt::ArrayInt(int size) : size(size), lastIndex(0) {
+ArrayInt::ArrayInt(int size) : size(size), lastIndex(0)
+{
     theArray = new int[size];
 }
 
 // destructor, delete the array and go away
-ArrayInt::~ArrayInt() { delete[] theArray; }
+ArrayInt::~ArrayInt()
+{
+    delete[] theArray;
+}
 
 // add value at next location
 // call resize to resize array if full
-void ArrayInt::append(int value) {
-    return;
+void ArrayInt::append(int value)
+{
+    lastIndex++;
+    theArray[lastIndex] = value;
 }
 
 // get value at last location
 // throw exception if empty
-int ArrayInt::getLast() {
-    return 7;
+int ArrayInt::getLast()
+{
+
+    int value;
+    if (lastIndex <= 0)
+    {
+        throw std::out_of_range("Array is empty!");
+    }
+    else
+    {
+        value = theArray[lastIndex];
+    }
+
+    return value;
 }
 
 // remove value at last location
 // does nothing if empty
-void ArrayInt::deleteLast() {
-    return;
+void ArrayInt::deleteLast()
+{
+    if (lastIndex <= 0)
+    {
+        throw std::out_of_range("Array is empty!");
+    }
+    else
+    {
+        if (lastIndex < 1)
+        {
+            lastIndex = 1;
+        }
+
+        theArray[lastIndex--] = ' ';
+    }
+
 }
 
 // create new array of newSize and copy to it
-void ArrayInt::resize(int newSize) {
-    return;
+void ArrayInt::resize(int newSize)
+{
+    int* newArray = new int[newSize];
+
+    std::copy(theArray, theArray + size, newArray);
+
+    delete[] theArray;
+    theArray = newArray;
 }
 
 // list the elements from 0 to lastIndex
-std::string ArrayInt::listElements() {
-    return "dummy string";
+std::string ArrayInt::listElements()
+{
+    if (lastIndex <= 0)
+    {
+        return "Array is empty!";
+    }
+    for (int i = 0; i < lastIndex; i++)
+    {
+        std::cout << ' ' << theArray[i];
+    }
 }
 
 // find a value if present
-bool ArrayInt::find(int value) {
+bool ArrayInt::find(int value)
+{
     return false;
 }
 
 // remove a value if found
-bool ArrayInt::removeVal(int value) {
+bool ArrayInt::removeVal(int value)
+{
     return false;
 }
 
 // find and return the largest value
-int ArrayInt::findLargest() {
+int ArrayInt::findLargest()
+{
     return 7;
 }
 
 // remove largest value
-void ArrayInt::removeLargest() {
+void ArrayInt::removeLargest()
+{
     return;
 }
 
 // insert into array at specified location
-void ArrayInt::insertAt(int index, int value) {
+void ArrayInt::insertAt(int index, int value)
+{
     return;
 }
 
 // remove an item and compress the array
-int ArrayInt::removeAt(int index) {
+int ArrayInt::removeAt(int index)
+{
     return 7;
 }
 
 // use append, remove, and findLargest to arrange values in descending order
-void ArrayInt::solveThink(int *values, int numValues) {
+void ArrayInt::solveThink(int *values, int numValues)
+{
     return;
 }
 
